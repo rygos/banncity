8839524f9e9cee57eb00adc45c560f7503d5cac9
Marcel Hering <ryg@banncity.de>
2016-08-11 06:07:05 -0400

    Applied fixes from StyleCI

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,13 @@
 /*
  * This file is part of Hifone.
  *
- * (c) Hifone.com <hifone@hifone.com>
+ * (c) until 2016-08-11 Hifone.com <hifone@hifone.com>
+ * (c) changes made after 2016-08-11 banncity.de <broxxer@broxxer.de>
  *
- * For the full copyright and license information, please view the LICENSE
+ * For the ful copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 
 namespace Hifone\Http\Controllers;
 
@@ -30,7 +32,7 @@ class HomeController extends Controller
         if (Auth::check()) {
             $activeDate = app('session')->get('active_date');
 
-            if (!$activeDate || $activeDate != date('Ymd')) {
+            if (! $activeDate || $activeDate != date('Ymd')) {
                 event(new UserWasLoggedinEvent(Auth::user()));
                 app('session')->put('active_date', date('Ymd'));
             }
