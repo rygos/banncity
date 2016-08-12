d888d4f74ce8091c9d838c86d580def30bbf04eb
Marcel Hering <ryg@banncity.de>
2016-08-12 04:38:01 -0400

    Applied fixes from StyleCI

--- database/migrations/2014_04_02_193005_create_translations_table.php
+++ database/migrations/2014_04_02_193005_create_translations_table.php
@@ -1,19 +1,28 @@
 <?php
 
+/*
+ * This file is part of Hifone.
+ *
+ * (c) until 2016-08-11 Hifone.com <hifone@hifone.com>
+ * (c) changes made after 2016-08-11 banncity.de <broxxer@broxxer.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTranslationsTable extends Migration {
-
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-        Schema::create('ltm_translations', function(Blueprint $table)
-        {
+class CreateTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ltm_translations', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('status')->default(0);
             $table->string('locale');
@@ -22,16 +31,15 @@ class CreateTranslationsTable extends Migration {
             $table->text('value')->nullable();
             $table->timestamps();
         });
-	}
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
         Schema::drop('ltm_translations');
-	}
-
+    }
 }
--- resources/lang/fr/layouts.php
+++ resources/lang/fr/layouts.php
@@ -1,8 +1,17 @@
 <?php
 
-return array (
-  'default' => 
-  array (
+/*
+ * This file is part of Hifone.
+ *
+ * (c) until 2016-08-11 Hifone.com <hifone@hifone.com>
+ * (c) changes made after 2016-08-11 banncity.de <broxxer@broxxer.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return  [
+  'default' =>  [
     'yes' => 'Oui',
-  ),
-);
+  ],
+];
